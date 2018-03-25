fc3ccc42df3943bd8e29cd77338e77c265677ea2
luloop <lucilarizzi@gmail.com>
2018-03-25 18:20:40 -0300

    domingo

---
+++ Clase_02/TAREA/260318-TAREA/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ /*
+    int lado;
+    int perimetro;
+    printf("Ingrese la medida del lado del triangulo equilatero\n");
+    scanf( "%d", &lado);
+
+perimetro= lado*3;
+
+printf("el perimetro es: %d", perimetro);
+
+*/
+/*
+int ancho;
+int largo;
+int alambrado;
+
+printf(" ingrese el ancho del terreno \n");
+scanf("%d", &ancho);
+
+printf(" ingrese el largo del terreno \n");
+scanf("%d", &largo);
+
+alambrado= ((ancho+largo)*2)*6;
+
+printf("usted necesita %d metros de alambrado", alambrado); */
+
+
+long char mes;
+ switch ()
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
