5c53dff23496f966bee36a007c8f7bcdbf82e7c1
Lucila Rizzi <36158159+luloop@users.noreply.github.com>
2018-03-26 06:38:40 -0300

    lunes

--- Clase_02/TAREA/260318-TAREA/main.c
+++ Clase_02/TAREA/260318-TAREA/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h> //biblioteca de getche y getch
+#include <ctype.h> //biblioteca de funciones
+#include <math.h>
 
 int main()
 { /*
@@ -28,18 +31,55 @@ alambrado= ((ancho+largo)*2)*6;
 
 printf("usted necesita %d metros de alambrado", alambrado); */
 
+/*
+char mes;
+printf("ingrese la inicial del mes \n" );
+mes= tolower( getche());
+ switch (mes)
+ {
+ case 'e':
+ case 'f':
+ printf("\n veranito");
+ break;
+ default:
+ printf("\n extraño el veranito\n\n\n\n\n");
+ break;
+ }
+ */
 
-long char mes;
- switch ()
+ int venta;
+ int maximo;
+ int minimo;
+ int flag=0;
+ int dia =1;
+ int sumatotal=0;
 
 
+ for (int i=0 ; i<7 ; i++)
+ {
+ printf(" ingrese el importe del dia %d \n", dia);
+ scanf("%d", &venta);
+ while (venta <=0 )
+ {
+     printf(" REingrese el importe del dia %d \n", dia);
+    scanf("%d", &venta);
+ }
+ dia ++;
+ sumatotal= sumatotal+ venta;
 
+ if (venta > maximo || flag == 0)
+ {
+     maximo = venta;
 
+ }
+ if(venta < minimo || flag == 0)
+ {
+     minimo= venta;
+     flag=1;
+ }
+ }
 
-
-
-
-
+ printf(" el importe total de lo 7 dias es %d \n el maximo es %d \n y el minimo es %d",sumatotal,maximo,minimo);
 
     return 0;
 }
